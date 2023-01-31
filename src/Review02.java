9996514cc0d9428517b3c402b75b1b1474f4818b
AkikoWada <ak.wada@gmail.com>
2023-01-31 17:36:16 +0900

    Lesson4課題提出

---
+++ src/Review02.java
@@ -0,0 +1,22 @@
+
+public class Review02 {
+
+    public static void main(String[] args) {
+        // iの値が0から21より小さい間、処理を繰り返す
+        for (int i=0; i<101; i++) {
+            // iの値が3でも5でも割り切れるとき
+            if (i%3==0 && i%5==0) {
+                System.out.println("FizzBuzz");
+            // iの値が3で割り切れるとき
+            }else if (i%3==0) {
+                    System.out.println("Fizz");
+            // iの値が5で割り切れるとき
+            }else if (i%5==0) {
+                System.out.println("Buzz");
+            }else {
+                System.out.println(i);
+            }
+         }
+    }
+}
+
